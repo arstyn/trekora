a6701b936c7cdb39b942c4d147d8eaa5ddb25ecb
faiz-basys <faizashrafkk@gmail.com>
2025-05-08 02:45:43 +0530

    feat: user entity relation added

--- apps/backend/src/user/entity/user.entity.ts
+++ apps/backend/src/user/entity/user.entity.ts
@@ -1,9 +1,13 @@
+import { Branch } from 'src/branch/entity/branch.entity';
+import { Organization } from 'src/organization/entity/organization.entity';
 import {
   Entity,
   PrimaryGeneratedColumn,
   Column,
   CreateDateColumn,
   UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
 } from 'typeorm';
 
 @Entity('user')
@@ -11,8 +15,19 @@ export class User {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column('uuid', { nullable: true })
-  branch_id?: string;
+  @Column('uuid', { nullable: true, name: 'branch_id' })
+  branchId?: string;
+
+  @Column('uuid', { nullable: true, name: 'organization_id' })
+  organizationId?: string;
+
+  @ManyToOne(() => Branch, { nullable: true, onDelete: 'SET NULL' })
+  @JoinColumn({ name: 'branch_id' })
+  branch?: Branch;
+
+  @ManyToOne(() => Organization, { nullable: true, onDelete: 'SET NULL' })
+  @JoinColumn({ name: 'organization_id' })
+  organization?: Organization;
 
   @Column({ unique: true })
   email: string;
@@ -26,12 +41,12 @@ export class User {
   @Column('uuid', { nullable: true })
   role_id?: string;
 
-  @CreateDateColumn({ type: 'timestamp' })
-  created_at: Date;
+  @CreateDateColumn({ type: 'timestamp', name: 'created_at' })
+  createdAt: Date;
 
-  @UpdateDateColumn({ type: 'timestamp' })
-  updated_at: Date;
+  @UpdateDateColumn({ type: 'timestamp', name: 'updated_at' })
+  updatedAt: Date;
 
-  @Column({ default: true })
-  is_active: boolean;
+  @Column({ default: true, name: 'is_active' })
+  isActive: boolean;
 }
