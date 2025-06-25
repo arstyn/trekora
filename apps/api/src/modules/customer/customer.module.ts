fd1da7b6b8bcab485e2efabb13873e3a8e05fcf2
faiz-basys <faizashrafkk@gmail.com>
2025-06-26 01:31:13 +0530

    fix: jwt import in customer module

--- apps/api/src/modules/customer/customer.module.ts
+++ apps/api/src/modules/customer/customer.module.ts
@@ -3,9 +3,10 @@ import { CustomerController } from './customer.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CustomerService } from './customer.service';
 import { Customer } from './entity/customer.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Customer])],
+  imports: [TypeOrmModule.forFeature([Customer]), JwtModule.register({})],
   controllers: [CustomerController],
   providers: [CustomerService],
 })
