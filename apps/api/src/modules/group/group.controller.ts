44a30bdf4a61152110c17f1baa74934200dced17
Sayed Sinan Umer Jifry <107292770+sayedsinan@users.noreply.github.com>
2025-06-30 18:35:03 +0530

    group also added

---
+++ apps/api/src/modules/group/group.controller.ts
@@ -0,0 +1,37 @@
+import { Controller, Get, Post, Body, Param, Put, Delete, UseGuards } from '@nestjs/common';
+import { GroupService } from './group.service';
+import { CreateGroupDto } from './dto/create-group';
+import { UpdateGroupDto } from './dto/update-group';
+import { Group } from './entity/group.entity';
+import { AuthGuard } from '../auth/guard/auth.guard';
+
+@UseGuards(AuthGuard)
+@Controller('api/groups')
+export class GroupController {
+  constructor(private readonly groupService: GroupService) {}
+
+  @Post()
+  create(@Body() dto: CreateGroupDto): Promise<Group> {
+    return this.groupService.create(dto);
+  }
+
+  @Get()
+  findAll(): Promise<Group[]> {
+    return this.groupService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Promise<Group> {
+    return this.groupService.findOne(+id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() dto: UpdateGroupDto): Promise<Group> {
+    return this.groupService.update(id, dto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number): Promise<void> {
+    return this.groupService.delete(id);
+  }
+}
