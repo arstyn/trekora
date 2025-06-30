c4f635f09afacadce832d2d1e48e2a60b3883a3d
faiz-basys <faizashrafkk@gmail.com>
2025-06-30 23:59:03 +0530

    fix: jwt import

--- apps/api/src/modules/group/group.module.ts
+++ apps/api/src/modules/group/group.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Group } from './entity/group.entity';
 import { GroupService } from './group.service';
 import { GroupController } from './group.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Group])],
+  imports: [TypeOrmModule.forFeature([Group]), JwtModule.register({})],
   providers: [GroupService],
   controllers: [GroupController],
 })
