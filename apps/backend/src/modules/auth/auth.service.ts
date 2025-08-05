8d26c3769eb5996883f37917474fe35b88027692
faiz-basys <faizashrafkk@gmail.com>
2025-08-05 20:30:03 +0530

    cleanup: consoles removed

--- apps/backend/src/modules/auth/auth.service.ts
+++ apps/backend/src/modules/auth/auth.service.ts
@@ -240,14 +240,6 @@ export class AuthService {
 
   // Login functionality
   async login(email: string, password: string): Promise<ILoginResponse> {
-    console.log(
-      '🚀 ~ auth.service.ts:245 ~ AuthService ~ login ~ password:',
-      password,
-    );
-    console.log(
-      '🚀 ~ auth.service.ts:245 ~ AuthService ~ login ~ email:',
-      email,
-    );
     const user = await this.userService.findOneWithEmail(email);
     if (!user) throw new UnauthorizedException('Invalid credentials');
 
