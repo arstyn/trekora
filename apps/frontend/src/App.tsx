a475dbe06ef1112b60250f91d4171ea885e17b47
faiz-basys <faizashrafkk@gmail.com>
2025-08-15 03:33:49 +0530

    feat: remove create extra route

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -48,7 +48,6 @@ function AuthenticatedApp() {
 					<Route path="/leads" element={<Leads />} />
 					<Route path="/packages" element={<Packages />} />
 					<Route path="/packages/create" element={<CreatePackagePage />} />
-					<Route path="/packages/create/:id" element={<CreatePackagePage />} />
 					<Route path="/packages/edit/:id" element={<EditPackagePage />} />
 					<Route path="/packages/:id" element={<ViewPackagePage />} />
 					<Route path="/customers" element={<CustomerManagement />} />
